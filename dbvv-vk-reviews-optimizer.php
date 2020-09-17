211955b6692751a6a08176701177ce56c5b6bb21
Daniil Borovkov <daniilborowkow@gmail.com>
2020-09-17 16:15:13 +0600

    Initial commit

---
+++ dbvv-vk-reviews-optimizer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Dbvv Vk Reviews Optimizer
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     dbvv-vk-reviews-optimizer
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Dbvv_Vk_Reviews_Optimizer
+ */
+
+// Your code starts here.
